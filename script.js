32c4e615627d05fcae11c90b6fda5cbbace9cbba
YousifBurhan <113238747+YousifBurhan@users.noreply.github.com>
2022-10-01 00:59:21 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+document.getElementById('button').addEventListener('click', (event) => {
+  let element_list = document.getElementById('list');
+  let new_li = document.createElement('li');
+  new_li.innerText = '';
+
+  element_list.appendChild(new_li);
+
+});
